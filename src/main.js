468003df9f2f3eb31ba4b174af8e398941c856a7
Newt Novak <manovak@ucsc.edu>
2022-04-20 12:01:38 -0700

    withmessage

--- src/main.js
+++ src/main.js
@@ -11,3 +11,17 @@ let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize / 3;
 // reserve keyboard vars
 let keyF, keyR, keyLEFT, keyRIGHT
+
+/*
+PROJECT TITLE: FISHAGGEDON (ROCKET PATROL MOD)
+CREATOR: Newton Novak
+TIME SPENT WORKING: 12 hours
+POINT BREAKDOWN:
+Redesign the game's artwork, UI, and sound to change its theme/aesthetic (to something other than sci-fi) (60)
+Create a new animated sprite for the Spaceship enemies (10)
+Display the time remaining (in seconds) on the screen (10)
+Implement parallax scrolling (10
+Allow the player to control the Rocket after it's fired (5)
+Randomize each spaceship's movement direction at the start of each play (5)
+TOTAL POINTS: 100
+*/
